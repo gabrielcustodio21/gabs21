890f1d323498410be0882b4818a2c4dc44b09058
Gabriel Custodio <gkustodor@gmail.com>
2021-05-16 10:24:18 -0300

    Setting folders to repository.

--- hello.js
+++
@@ -1,7 +0,0 @@
-var x = 3
-console.log("hello world");
-
-    if (x >= 3) {
-        x = x + 1;
-    }
-console.log(x)
--- hw.py
+++
@@ -1,5 +0,0 @@
-x = 3
-print("hello world!")
-if x >=3:
-    x = x + 1
-print(x)
