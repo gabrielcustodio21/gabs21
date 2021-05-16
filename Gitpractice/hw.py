de8fc44f3196b1068952777d2be60958fd6930f2
Gabriel Custodio <gkustodor@gmail.com>
2021-05-16 13:41:13 -0300

    Now we have an interactive site.

--- Gitpractice/hw.py
+++ Gitpractice/hw.py
@@ -1,10 +1,10 @@
 x = 1
 contador = 0
 while x <= 1000:
-    if not x % 2 == 0 and not x % 3 == 0:
+    if x % 2 == 0 or x % 3 == 0:
         contador = contador + 1
         x = x+1
     else:
-        x = x+1
+        x =x+1
  
 print (contador)
