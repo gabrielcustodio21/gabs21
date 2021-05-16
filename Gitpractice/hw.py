7bb183a3530ab3c7690c8cf9a5540deb827bd45f
Gabriel Custodio <gkustodor@gmail.com>
2021-05-16 13:21:54 -0300

    Just practicing.

--- Gitpractice/hw.py
+++ Gitpractice/hw.py
@@ -1,5 +1,10 @@
-x = 3
-print("hello world!")
-if x >=3:
-    x = x + 1
-print(x)
+x = 1
+contador = 0
+while x <= 1000:
+    if not x % 2 == 0 and not x % 3 == 0:
+        contador = contador + 1
+        x = x+1
+    else:
+        x = x+1
+ 
+print (contador)
